b8c4e2a3ef35123d82d4787a12a8a39bb30c14cd
MiniMathMan <Home@Lisas-MacBook-Pro-2.local>
2014-07-01 13:01:35 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.m
@@ -0,0 +1,24 @@
+#import "Grid.h"
+#import "Creature.h"
+
+// these are variables that cannot be changed
+static const int GRID_ROWS = 8;
+static const int GRID_COLUMNS = 10;
+
+@implementation Grid {
+    NSMutableArray *_gridArray;
+    float _cellWidth;
+    float _cellHeight;
+}
+
+- (void)onEnter
+{
+    [super onEnter];
+    
+    [self setupGrid];
+    
+    // accept touches on the grid
+    self.userInteractionEnabled = YES;
+}
+
+@end

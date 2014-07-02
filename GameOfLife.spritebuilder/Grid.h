ba4ebb914bcf2be747ae0062bbb3919816dd3d91
MiniMathMan <Home@Lisas-MacBook-Pro-2.local>
2014-07-02 10:17:52 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -14,5 +14,12 @@
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
 
+-(void) evolveStep;
+
+-(void) countNeighbors;
+
+-(void) updateCreatures;
+
+- (BOOL)isIndexValidForX:(int)x andY:(int)y;
 
 @end
